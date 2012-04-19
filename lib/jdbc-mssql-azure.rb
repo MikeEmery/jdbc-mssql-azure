602c923da8a83fb8e5b9012212867a2721155dcf
Mike Emery <philodoxx@gmail.com>
2012-04-19 12:33:12 -0700

    first commit

---
+++ lib/jdbc-mssql-azure.rb
@@ -0,0 +1,9 @@
+require "jdbc-mssql-azure/version"
+
+module Jdbc
+  module Mssql
+    module Azure
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jdbc-mssql-azure/version.rb
@@ -0,0 +1,7 @@
+module Jdbc
+  module Mssql
+    module Azure
+      VERSION = "0.0.1"
+    end
+  end
+end
